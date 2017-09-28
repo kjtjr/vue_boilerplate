f108fb1737ccf6a221d3eae3e9982878bf77c2db
Ken Trinka <d865761@dartcontainer.com>
2017-09-28 10:15:24 -0400

    Added ES6 Promise to fix IE Issues

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,5 @@
+import es6Promise from 'es6-promise'
+es6Promise.polyfill()
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { mutations } from './mutations'
@@ -5,6 +7,7 @@ import { getters } from './getters'
 import { actions } from './actions'
 import lodash from 'lodash'
 import VueLodash from 'vue-lodash/dist/vue-lodash.min'
+import moment from 'moment'
 
 Vue.use(Vuex, VueLodash, lodash)
 
